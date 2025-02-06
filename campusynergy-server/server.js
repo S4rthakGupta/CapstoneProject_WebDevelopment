28df8b7276a2c061b87bf27adc5970460dda60c6
S4rthakGupta <sarthakgupta9814@gmail.com>
2025-02-06 18:18:56 -0500

    c

--- campusynergy-server/server.js
+++ campusynergy-server/server.js
@@ -15,5 +15,7 @@ app.use(express.json());
 // Routes
 app.use('/api/users', require('./routes/userRoutes'));
 
+// console.log("Test")
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));

06d24ab23ea7953cc740432b1d4b9816a5ccb671
Dilpreet Kaur <dhanjaldilpreet1@gmail.com>
2025-02-06 18:26:41 -0500

    comment added

--- campusynergy-server/server.js
+++ campusynergy-server/server.js
@@ -16,6 +16,7 @@ app.use(express.json());
 app.use('/api/users', require('./routes/userRoutes'));
 
 // console.log("Test")
+// dilpreet
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));

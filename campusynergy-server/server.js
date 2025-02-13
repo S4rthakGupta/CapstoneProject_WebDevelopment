43e1fcac934c4ef938100b44fef379fd412e724a
Shakila101 <167652608+101Shakila@users.noreply.github.com>
2025-02-13 12:16:38 -0500

    change in port

--- campusynergy-server/server.js
+++ campusynergy-server/server.js
@@ -16,5 +16,5 @@ app.use(express.json());
 app.use('/api/users', require('./routes/userRoutes'));
 
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));

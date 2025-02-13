b69145df98ffdb1b6e7f4562018fc64e8231ed85
Shakila101 <167652608+101Shakila@users.noreply.github.com>
2025-02-13 11:40:14 -0500

    Revert "notes"
    This reverts commit 51931d049f9cc2c990b28d97d7d77d6fdc2c98f5.

--- campusynergy-server/server.js
+++ campusynergy-server/server.js
@@ -1,20 +1,20 @@
 const express = require('express');
-const connectDB = require('./config/db');  // Import MongoDB connection function
+const connectDB = require('./config/db');
 const cors = require('cors');
-require('dotenv').config(); // Load environment variables from .env
+require('dotenv').config();
 
 const app = express();
 
 // Connect to MongoDB
-connectDB();  // Calls the function in db.js to establish the database connection
+connectDB();
 
 // Middleware
-app.use(cors()); // Enables Cross-Origin Resource Sharing
-app.use(express.json()); // Allows parsing JSON request bodies
+app.use(cors());
+app.use(express.json());
 
 // Routes
-app.use('/api/users', require('./routes/userRoutes')); // Load user routes
+app.use('/api/users', require('./routes/userRoutes'));
 
-// Start Server
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));

f6d557650e88a699ba76b097bb6c88c30e0bba2d
Dilpreet Kaur <dhanjaldilpreet1@gmail.com>
2025-02-20 19:47:42 -0500

    login UI created

--- campusynergy-client/src/components/Footer.js
+++ campusynergy-client/src/components/Footer.js
@@ -1,16 +1,41 @@
-import React from 'react';
+import React from "react";
 
 const Footer = () => {
-    return (
-        <footer className="footer mt-auto">
-            <p>&copy; {new Date().getFullYear()} CampuSynergy | All rights reserved.</p>
-            <div>
-                <a href="https://twitter.com" target="_blank" rel="noopener noreferrer" className="text-white mx-2">Twitter</a> |
-                <a href="https://facebook.com" target="_blank" rel="noopener noreferrer" className="text-white mx-2">Facebook</a> |
-                <a href="https://instagram.com" target="_blank" rel="noopener noreferrer" className="text-white mx-2">Instagram</a>
-            </div>
-        </footer>
-    );
+  return (
+    <footer className="footer mt-auto">
+      <p>
+        &copy; {new Date().getFullYear()} CampuSynergy | All rights reserved.
+      </p>
+      <div>
+        <a
+          href="https://twitter.com"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="text-black mx-2"
+        >
+          Twitter
+        </a>{" "}
+        |
+        <a
+          href="https://facebook.com"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="text-black mx-2"
+        >
+          Facebook
+        </a>{" "}
+        |
+        <a
+          href="https://instagram.com"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="text-black mx-2"
+        >
+          Instagram
+        </a>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
